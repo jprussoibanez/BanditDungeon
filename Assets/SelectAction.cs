c3164afd836dc01234b1b2bf749b0e91d3bb302c
jprusso <jprussoibanez@gmail.com>
2022-02-06 21:38:01 -0300

    Clean up and improve code.

--- Assets/SelectAction.cs
+++ Assets/SelectAction.cs
@@ -2,57 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectAction : MonoBehaviour {
+public class SelectAction : MonoBehaviour
+{
+    public float myProbability;
+    private const float rewardWin = 1.0f;
+    private const float rewardLose = -0.5f;
 
-	public float myProb;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	public float Selected() {
-		float reward = 0f;
-		if (myProb > Random.Range (0.0f, 1.0f)) {
-			GameObject diamond = (GameObject)GameObject.Instantiate (Resources.Load ("diamond"));
-			diamond.transform.position = this.transform.position;
-			diamond.name = "diamond";
-			reward = 1.0f;
-			foreach (GameObject chest in GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().chests) {
-				if (chest == this.gameObject) {
-					chest.gameObject.GetComponent<MeshRenderer> ().enabled = false;
-				}
-				chest.gameObject.GetComponent<Collider> ().enabled = false;
-			}
-		} else {
-			GameObject slime = (GameObject)GameObject.Instantiate (Resources.Load ("slime"));
-			slime.transform.position = this.transform.position;
-			slime.name = "slime";
-			reward = -1.0f;
-			foreach (GameObject chest in GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().chests) {
-				if (chest == this.gameObject) {
-					chest.gameObject.GetComponent<MeshRenderer> ().enabled = false;
-				}
-				chest.gameObject.GetComponent<Collider> ().enabled = false;
-			}
-		}
-		StartCoroutine (Example ());
-		return reward;
-	}
+    }
 
-	void OnMouseDown() {
-		float r = Selected ();
-		GameObject.Find ("Main Camera").GetComponent<BanditEnvironment> ().totalRewards += r;
-	}
+    public float Selected()
+    {
+        float reward = 0f;
+        if (myProbability > Random.Range(0.0f, 1.0f))
+        {
+            reward = rewardWin;
+            ShowResult(BanditEnvironment.ResourceDiamond);
+        }
+        else
+        {
+            reward = rewardLose;
+            ShowResult(BanditEnvironment.ResourceSlime);
+        }
+        StartCoroutine(Example());
+        return reward;
+    }
 
-	IEnumerator Example() {
-		yield return new WaitForSeconds(GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().actSpeed);
-		GameObject.Find ("Main Camera").GetComponent<BanditEnvironment> ().LoadTrial ();
-	}
+    private void ShowResult(string resource)
+    {
+        GameObject slime = (GameObject)GameObject.Instantiate(Resources.Load(resource));
+        slime.transform.position = this.transform.position;
+        slime.name = resource;
+        foreach (GameObject chest in GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().chests)
+        {
+            if (chest == this.gameObject)
+            {
+                chest.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+            chest.gameObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        float r = Selected();
+        GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().totalRewards += r;
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().actSpeed);
+        GameObject.Find("Main Camera").GetComponent<BanditEnvironment>().LoadTrial();
+    }
 
 
 }
